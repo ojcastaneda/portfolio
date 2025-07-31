758abe57226830a5efc3bc4874b2ef2a6633f1f8
Oscar Julián Castañeda Gutiérrez <ojcastaneda@hisode.org>
2025-07-30 19:37:19 -0500

    Fix locale

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',
-			strategy: ['preferredLanguage']
+			strategy: ['preferredLanguage', 'baseLocale']
 		})
 	]
 });
